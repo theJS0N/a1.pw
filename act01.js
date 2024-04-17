5be098a295f977d91e48e1998896d5eea9be2b3e
theJS0N <162645670+theJS0N@users.noreply.github.com>
2024-04-17 20:59:15 -0300

    Create act01.js

---
+++ act01.js
@@ -0,0 +1,25 @@
+// 1)
+var num1 = Number(prompt("Digite o primeiro número:"));
+var num2 = Number(prompt("Digite o segundo número:"));
+var num3 = Number(prompt("Digite o terceiro número:"));
+
+console.log("Os números digitados foram:");
+console.log("Número 1:", num1);
+console.log("Número 2:", num2);
+console.log("Número 3:", num3);
+// 2)
+var n1 = parseFloat(prompt("Digite o primeiro número:"));
+var n2 = parseFloat(prompt("Digite o segundo número:"));
+var n3 = parseFloat(prompt("Digite o terceiro número:"));
+var soma = n1 + n2 + n3;
+var media = soma / 3;
+
+console.log("A média dos números digitados é:", media);
+// 3)
+var numero1 = 10;
+var numero2 = 20;
+var numero3 = 30;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
